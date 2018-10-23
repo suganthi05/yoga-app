a6a492ce00aeca81545a8a073c87731986f2f4b8
ankitaahuja <ankitaahuja111@gmail.com>
2018-10-23 18:16:40 +0000

    heart animation added in the show page and favorite page

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     });
 
 });
-
+//login jquery
 $(document).ready(function () {
     $("#login-button").click(function (event) {
         event.preventDefault();
@@ -40,4 +40,36 @@ $(document).ready(function () {
         $('form').fadeOut(500);
         $('.wrapper').addClass('form-success');
     });
+});
+
+//code for add to my favs   
+$(document).ready(function () {
+
+    $('.click').click(function () {
+        if ($('span').hasClass("fa-star")) {
+            $('.click').removeClass('active')
+            setTimeout(function () {
+                $('.click').removeClass('active-2')
+            }, 30)
+            $('.click').removeClass('active-3')
+            setTimeout(function () {
+                $('span').removeClass('fa-star')
+                $('span').addClass('fa-star-o')
+            }, 15)
+        } else {
+            $('.click').addClass('active')
+            $('.click').addClass('active-2')
+            setTimeout(function () {
+                $('span').addClass('fa-star')
+                $('span').removeClass('fa-star-o')
+            }, 150)
+            setTimeout(function () {
+                $('.click').addClass('active-3')
+            }, 150)
+            $('.info').addClass('info-tog')
+            setTimeout(function () {
+                $('.info').removeClass('info-tog')
+            }, 1000)
+        }
+    })
 });

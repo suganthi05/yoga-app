90d299ae19aa515f3c8a81f0536dd1ee57336e1e
ankitaahuja <ankitaahuja111@gmail.com>
2018-10-24 18:29:57 +0000

    multiple pointers fixed in profile page

--- public/scripts/userProfile.js
+++ public/scripts/userProfile.js
@@ -7,12 +7,12 @@ function initMap() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
+    console.log("userFavLocations: " + userFavLocations);
     let marker = new google.maps.Marker({
         position: new google.maps.LatLng(userLat, userLng),
         map: map,
         icon: {
-            url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+            url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
         }
 
     });
@@ -38,7 +38,35 @@ function addMarkers() {
             position: new google.maps.LatLng(yoga.lat, yoga.lng),
             map: map,
             icon: {
-                url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+                url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+            }
+        });
+
+        // console.log("yoga lat: " + yoga.lat);
+        // console.log("yoga Obj: " + JSON.stringify(yoga));
+        // console.log("yoga : " + yoga.name);
+        google.maps.event.addListener(marker, 'click', (function (marker, index) {
+            return function () {
+                let infowindow = new google.maps.InfoWindow();
+                infowindow.setContent(
+                    "<p><b>" + yoga.name + "</b></p>" +
+                    // "<p>" + yoga.location + "</p>" +
+                    "<img src='" + yoga.image + "' width='100px' height='80px'>")
+                // "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='/yoga/" + yoga._id + "' class='btn btn-primary'>More Info</a>")
+                infowindow.open(map, marker);
+            }
+        })(marker, index));
+
+    });
+
+
+    userStudiosLocations.forEach(yoga => {
+        ++index;
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(yoga.lat, yoga.lng),
+            map: map,
+            icon: {
+                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
             }
         });
         //console.log("yoga lat: " + yoga.lat);
@@ -57,31 +85,4 @@ function addMarkers() {
         })(marker, index));
 
     });
-
-
-    // userStudiosLocations.forEach(yoga => {
-    //     ++index;
-    //     marker = new google.maps.Marker({
-    //         position: new google.maps.LatLng(yoga.lat, yoga.lng),
-    //         map: map,
-    //         icon: {
-    //             url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-    //         }
-    //     });
-    //     //console.log("yoga lat: " + yoga.lat);
-    //     //console.log("yoga Obj: " + JSON.stringify(yoga));
-    //     //console.log("yoga : " + yoga.name);
-    //     google.maps.event.addListener(marker, 'click', (function (marker, index) {
-    //         return function () {
-    //             let infowindow = new google.maps.InfoWindow();
-    //             infowindow.setContent(
-    //                 "<p><b>" + yoga.name + "</b></p>" +
-    //                 // "<p>" + yoga.location + "</p>" +
-    //                 "<img src='" + yoga.image + "' width='100px' height='80px'>")
-    //             // "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='/yoga/" + yoga._id + "' class='btn btn-primary'>More Info</a>")
-    //             infowindow.open(map, marker);
-    //         }
-    //     })(marker, index));
-
-    // });
 }
